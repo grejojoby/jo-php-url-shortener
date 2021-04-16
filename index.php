d343dc7bdad0e3565486869b5db225c33f9d9a58
Grejo Joby <grejo00@gmail.com>
2021-04-16 15:26:27 +0530

    Final

--- index.php
+++ index.php
@@ -1,81 +1,114 @@
+<?php 
+  include "php/config.php";
+  $new_url = "";
+  if(isset($_GET)){
+    foreach($_GET as $key=>$val){
+      $u = mysqli_real_escape_string($conn, $key);
+      $new_url = str_replace('/', '', $u);
+    }
+      $sql = mysqli_query($conn, "SELECT full_url FROM url WHERE shorten_url = '{$new_url}'");
+      if(mysqli_num_rows($sql) > 0){
+        $sql2 = mysqli_query($conn, "UPDATE url SET clicks = clicks + 1 WHERE shorten_url = '{$new_url}'");
+        if($sql2){
+            $full_url = mysqli_fetch_assoc($sql);
+            header("Location:".$full_url['full_url']);
+          }
+      }
+  }
+?>
+
 <!DOCTYPE html>
+<!-- Coding By CodingNepal - youtube.com/codingnepal -->
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jo URL Shortener</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
-
+  <meta charset="UTF-8">
+  <title>URL Shortener in PHP | CodingNepal</title>
+  <link rel="stylesheet" href="style.css">
+  <!-- Iconsout Link for Icons -->
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
 </head>
 <body>
-    <div class="wrapper">
-        <form action="#">
-            <i class="url-icon uil uil-link"></i>
-            <input type="text" placeholder="Enter or paste a long url" required>
-            <button>Shorten</button>
-        </form>
-        <div class="count">
-            <span>Total Links: <span>10</span> | Total Clicks: <span>140</span></span>
-            <a href="#">Clear All</a>
+  <div class="wrapper">
+    <form action="#" autocomplete="off">
+      <input type="text" spellcheck="false" name="full_url" placeholder="Enter or paste a long url" required>
+      <i class="url-icon uil uil-link"></i>
+      <button>Shorten</button>
+    </form>
+    <?php
+      $sql2 = mysqli_query($conn, "SELECT * FROM url ORDER BY id DESC");
+      if(mysqli_num_rows($sql2) > 0){;
+        ?>
+          <div class="statistics">
+            <?php
+              $sql3 = mysqli_query($conn, "SELECT COUNT(*) FROM url");
+              $res = mysqli_fetch_assoc($sql3);
+
+              $sql4 = mysqli_query($conn, "SELECT clicks FROM url");
+              $total = 0;
+              while($count = mysqli_fetch_assoc($sql4)){
+                $total = $count['clicks'] + $total;
+              }
+            ?>
+            <span>Total Links: <span><?php echo end($res) ?></span> & Total Clicks: <span><?php echo $total ?></span></span>
+            <a href="php/delete.php?delete=all">Clear All</a>
         </div>
         <div class="urls-area">
-            <div class="title">
-                <li>Shorten URL</li>
-                <li>Original URL</li>
-                <li>Clicks</li>
-                <li>Actions</li>
-            </div>
-            <div class="data">
-                <li><a href="#">example.com/xyz123</a></li>
-                <li>https://google.com/search=sdakjfnaksnsfasjkfsdfasfs</li>
-                <li>16</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-            
-            <div class="data">
-                <li><a href="#">example.com/xyz123</a></li>
-                <li>https://google.com/search=sdakjfnaksnsfasjkfsdfasfs</li>
-                <li>16</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-            
-            <div class="data">
-                <li><a href="#">example.com/xyz123</a></li>
-                <li>https://google.com/search=sdakjfnaksnsfasjkfsdfasfs</li>
-                <li>16</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-            
-            <div class="data">
-                <li><a href="#">example.com/xyz123</a></li>
-                <li>https://google.com/search=sdakjfnaksnsfasjkfsdfasfs</li>
-                <li>16</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-            
-            <div class="data">
-                <li><a href="#">example.com/xyz123</a></li>
-                <li>https://google.com/search=sdakjfnaksnsfasjkfsdfasfs</li>
-                <li>16</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-        </div>
-    </div>
-    <div class="blur-effect"></div>
-    <div class="popup-box">
-        <div class="info-box error">
-            Your short link is ready. You can edit your short link now but can't edit once you saved it.
-        </div>
-        <form action="#">
-            <label>Edit your shorten url</label>
-            <input type="text" class="shorten-url" spellcheck="false" required value="example.com/xyz123">
-            <i class="copy-icon uil uil-copy-alt"></i>
-            <button>Save</button>
-        </form>
-    </div>
+          <div class="title">
+            <li>Shorten URL</li>
+            <li>Original URL</li>
+            <li>Clicks</li>
+            <li>Action</li>
+          </div>
+          <?php
+            while($row = mysqli_fetch_assoc($sql2)){
+              ?>
+                <div class="data">
+                <li>
+                  <a href="<?php echo $domain.$row['shorten_url'] ?>" target="_blank">
+                  <?php
+                    if($domain.strlen($row['shorten_url']) > 50){
+                      echo $domain.substr($row['shorten_url'], 0, 50) . '...';
+                    }else{
+                      echo $domain.$row['shorten_url'];
+                    }
+                  ?>
+                  </a>
+                </li> 
+                <li>
+                  <?php
+                    if(strlen($row['full_url']) > 60){
+                      echo substr($row['full_url'], 0, 60) . '...';
+                    }else{
+                      echo $row['full_url'];
+                    }
+                  ?>
+                </li> 
+              </li>
+                <li><?php echo $row['clicks'] ?></li>
+                <li><a href="php/delete.php?id=<?php echo $row['shorten_url'] ?>">Delete</a></li>
+              </div>
+              <?php
+            }
+          ?>
+      </div>
+        <?php
+      }
+    ?>
+  </div>
 
-    <script src="script.js"></script>
+  <div class="blur-effect"></div>
+  <div class="popup-box">
+  <div class="info-box">Your short link is ready. You can also edit your short link now but can't edit once you saved it.</div>
+  <form action="#" autocomplete="off">
+    <label>Edit your shorten url</label>
+    <input type="text" class="shorten-url" spellcheck="false" required>
+    <i class="copy-icon uil uil-copy-alt"></i>
+    <button>Save</button>
+  </form>
+  </div>
+
+  <script src="script.js"></script>
+
 </body>
 </html>
+
